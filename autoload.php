f1db9471bf74144d0cb93af83d0bf36d3ffd772a
disable map scroll <dngege@gmail.com>
2019-04-05 17:18:12 +0200

    refactor autoload

--- autoload.php
+++ autoload.php
@@ -4,13 +4,11 @@
 // If the given class has the namespace we want, and
 // the file exists we include it automatically.
 spl_autoload_register(function ($class) {
-    if (! strpos($class, 'Pine\\SimplePay\\') === 0) {
+    if (strpos($class, 'Pine\\SimplePay\\') !== 0) {
         return;
     }
 
-    $file = simplepay_gateway_path(sprintf(
-        'src/%s.php', str_replace(['Pine\\SimplePay\\', '\\'], ['', '/'], $class)
-    ));
+    $file = __DIR__ . sprintf('/src/%s.php', str_replace(['Pine\\SimplePay\\', '\\'], ['', '/'], $class));
 
     if (file_exists($file)) {
         require_once $file;
