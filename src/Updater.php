546bc36c124c846fd50629c016ae6ce184e6d032
Gege <dngege@gmail.com>
2019-07-11 10:50:55 +0200

    update vars

--- src/Updater.php
+++ src/Updater.php
@@ -16,39 +16,39 @@ class Updater
     /**
      * Get the plugin info.
      *
-     * @param  array  $infoponse
+     * @param  array  $response
      * @param  string  $action
      * @param  object  $args
      * @return mixed
      */
-    public function info($infoponse, $action, $args)
+    public function info($response, $action, $args)
     {
         if ($action !== 'plugin_information') {
             return false;
         } elseif (Plugin::SLUG !== $args->slug) {
-		    return $infoponse;
-        } elseif (! $infoponse = get_transient('simplepay_update_' . Plugin::SLUG)) {
-            $remote = wp_remote_get($this->url);
+		    return $response;
+        } elseif (! $response = get_transient('simplepay_update_' . Plugin::SLUG)) {
+            $response = wp_remote_get($this->url);
 
-            if (wp_remote_retrieve_response_code($remote) == 200 && wp_remote_retrieve_body($remote)) {
-                set_transient('simplepay_update_' . Plugin::SLUG, $remote, 43200);
+            if (wp_remote_retrieve_response_code($response) == 200 && wp_remote_retrieve_body($response)) {
+                set_transient('simplepay_update_' . Plugin::SLUG, $response, 43200);
             }
         }
 
-        if ($remote) {
-            $remote = json_decode(wp_remote_retrieve_body($remote));
+        if ($response) {
+            $response = json_decode(wp_remote_retrieve_body($response));
 
             $info = new stdClass;
             $info->name = 'SimplePay Gateway for WooCommerce';
             $info->slug = Plugin::SLUG;
-            $info->version = str_replace('v', '', $remote->tag_name);
+            $info->version = str_replace('v', '', $response->tag_name);
             $info->tested = '5.2.2';
             $info->requires = '4.9.0';
             $info->author = '<a href="https://pineco.de">Pine</a>';
             $info->author_profile = 'https://pineco.de';
-            $info->download_link = $remote->zipball_url;
-            $info->trunk = $remote->zipball_url;
-            $info->last_updated = date('Y-m-d H:i:s', strtotime($remote->published_at));
+            $info->download_link = $response->zipball_url;
+            $info->trunk = $response->zipball_url;
+            $info->last_updated = date('Y-m-d H:i:s', strtotime($response->published_at));
             $info->sections = [];
 
             return $info;
@@ -67,28 +67,28 @@ class Updater
     {
         if (! $transient->checked) {
             return $transient;
-        } elseif (! $remote = get_transient('simplepay_update_' . Plugin::SLUG)) {
-            $remote = wp_remote_get($this->url);
+        } elseif (! $response = get_transient('simplepay_update_' . Plugin::SLUG)) {
+            $response = wp_remote_get($this->url);
 
-            if (wp_remote_retrieve_response_code($remote) == 200 && wp_remote_retrieve_body($remote)) {
-                set_transient('simplepay_update_' . Plugin::SLUG, $remote, 43200);
+            if (wp_remote_retrieve_response_code($response) == 200 && wp_remote_retrieve_body($response)) {
+                set_transient('simplepay_update_' . Plugin::SLUG, $response, 43200);
             }
         }
 
-        if ($remote) {
-            $remote = json_decode(wp_remote_retrieve_body($remote));
+        if ($response) {
+            $response = json_decode(wp_remote_retrieve_body($response));
 
-            if (version_compare(Plugin::VERSION, $version = str_replace('v', '', $remote->tag_name), '<')) {
+            if (version_compare(Plugin::VERSION, $version = str_replace('v', '', $response->tag_name), '<')) {
                 $info = new stdClass;
                 $info->slug = Plugin::SLUG;
                 $info->plugin = Plugin::SLUG;
                 $info->new_version = $version;
                 $info->tested = '5.2.2';
-                $info->package = $remote->zipball_url;
-                $info->url = $remote->html_url;
+                $info->package = $response->zipball_url;
+                $info->url = $response->html_url;
 
                 $transient->response[$info->plugin] = $info;
-                $transient->checked[$info->plugin] = $remote->version;
+                $transient->checked[$info->plugin] = $response->version;
             }
         }
 
