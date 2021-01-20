f16bea3df1834f169e2228f49420e84f79891992
Gergő D. Nagy <hello@iamgergo.com>
2021-01-20 12:10:30 +0100

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -3,7 +3,7 @@
 // The autoloader takes care of automatic file loading.
 // If the given class has the namespace we want, and
 // the file exists we include it automatically.
-spl_autoload_register(function ($class) {
+spl_autoload_register(static function ($class) {
     if (strpos($class, 'Pine\\SimplePay\\') !== 0) {
         return;
     }
